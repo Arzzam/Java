e731664463d1d2fc00f83ba8245ffdaaf84b3e46
Nafil Arzzam <arzzam19@gmail.com>
2022-12-21 21:24:39 +0530

    constructor part 2 - Constructor chaining

--- Lecture/src/Section7/BankAccount.java
+++ Lecture/src/Section7/BankAccount.java
@@ -9,6 +9,8 @@ public class BankAccount {
 
     //    Default Constructor
     public BankAccount() {
+        //  Using Constructor Chaining
+        this("DefaultName", 123456789L, 0, 987654321L, "defaultname@gmail.com");
         System.out.println("Empty constructor initialized");
     }
 
@@ -22,25 +24,17 @@ public class BankAccount {
         cusPhone = phoneNo;
     }
 
+//    Using 3 field in Constructor
+    public BankAccount(String cusName, String cusEmail, long cusPhone) {
+        this(cusName, 99999L, 10000, cusPhone, cusEmail);
+//        this.cusName = cusName;
+//        this.cusEmail = cusEmail;
+//        this.cusPhone = cusPhone;
+    }
+
     //    Getter for Bank Account
     public long getAccNo() {
         return accNo;
-    }
-
-    public int getAccBal() {
-        return accBal;
-    }
-
-    public String getCusName() {
-        return cusName;
-    }
-
-    public String getCusEmail() {
-        return cusEmail;
-    }
-
-    public long getCusPhone() {
-        return cusPhone;
     }
 
     //    Setter for Bank Account
@@ -48,16 +42,32 @@ public class BankAccount {
         this.accNo = accNo;
     }
 
+    public int getAccBal() {
+        return accBal;
+    }
+
     public void setAccBal(int accBal) {
         this.accBal = accBal;
+    }
+
+    public String getCusName() {
+        return cusName;
     }
 
     public void setCusName(String cusName) {
         this.cusName = cusName;
     }
 
+    public String getCusEmail() {
+        return cusEmail;
+    }
+
     public void setCusEmail(String cusEmail) {
         this.cusEmail = cusEmail;
+    }
+
+    public long getCusPhone() {
+        return cusPhone;
     }
 
     public void setCusPhone(long cusPhone) {
--- Lecture/src/Section7/Main.java
+++ Lecture/src/Section7/Main.java
@@ -2,10 +2,10 @@ package Section7;
 
 public class Main {
     public static void main(String[] args) {
-//        BankAccount acc = new BankAccount();
-        BankAccount acc = new BankAccount("Leethiyal", 234567891L,
-                1000, 9791588743L, "leethiyaldevaraj10@gmail.com");
-
+        BankAccount acc = new BankAccount();
+//        BankAccount acc = new BankAccount("Leethiyal", 234567891L,
+//                1000, 9791588743L, "leethiyaldevaraj10@gmail.com");
+        System.out.println(acc.getCusEmail());
         acc.setCusName("Nafil Arzzam");
         acc.setAccNo(23700156789L);
         acc.setAccBal(1000);
@@ -15,5 +15,8 @@ public class Main {
         acc.deposit(1000);
         acc.withdraw(1500);
         acc.withdraw(501);
+
+        BankAccount arzzamAcc = new BankAccount("Arzzam", "arzzam29@gmail.com", 943874374L);
+        System.out.println("Account no: " + arzzamAcc.getAccNo() + " Name: " + arzzamAcc.getCusName());
     }
 }
