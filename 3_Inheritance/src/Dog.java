f6aff778b2418b8328f022cb3e5525fa024f1b5d
Nafil Arzzam <arzzam19@gmail.com>
2022-12-25 20:53:39 +0530

    Inheritance part 2

---
+++ 3_Inheritance/src/Dog.java
@@ -0,0 +1,59 @@
+public class Dog extends Animal {
+
+    private String earShape;
+    private String tailShape;
+
+    public Dog() {
+        super("Mutt", "Big", 50);
+    }
+
+    public Dog(String type, double weight) {
+        this(type, weight, "Perky", "Curled");
+    }
+
+    public Dog(String type, double weight, String earShape, String tailShape) {
+        super(type, weight < 15 ? "small" : (weight < 35 ? "medium" : "large"), weight);
+        this.earShape = earShape;
+        this.tailShape = tailShape;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "earShape='" + earShape + '\'' +
+                ", tailShape='" + tailShape + '\'' +
+                "} " + super.toString();
+    }
+
+    public void makeNoise() {
+
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        if (speed == "slow") {
+            walk();
+            wagTail();
+        } else {
+            run();
+            bark();
+        }
+    }
+
+    public void bark() {
+        System.out.print("lol lol!! ");
+    }
+
+    public void run() {
+        System.out.print("Dog running ");
+    }
+
+    public void walk() {
+        System.out.print("Dog walking ");
+    }
+
+    public void wagTail() {
+        System.out.print("Waging tail ");
+    }
+}
--- 3_Inheritance/src/Main.java
+++ 3_Inheritance/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal, "slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, "fast");
+
+        Dog yorkie = new Dog("Yorkie", 15);
+        doAnimalStuff(yorkie, "fast");
+
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever, "slow");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        System.out.println("_ _ _ _ _");
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _ _");
     }
 }
